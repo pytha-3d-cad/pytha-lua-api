c5ec2182050bb625aced2c844ae632e76fbf6c8b
fabian-flassig <64731310+fabian-flassig@users.noreply.github.com>
2020-05-15 01:01:46 +0200

    Spiral Stairs 2 with Logic for number of steps

--- samples/Spiral Stairs 2/spiral_stairs2.lua
+++ samples/Spiral Stairs 2/spiral_stairs2.lua
@@ -8,8 +8,8 @@ function main()
 		step_thickness = 40,		--board thickness ot the steps
 		total_angle = 290,			--how much will the stairs wind? 360 is one full turn
 		rail_height = 900,			--height of the rail, measured to the center of the rail handle
-		diameter = 2400,			--diameter of the stairs to the center of the handrail. There is an additional offset (see outer_radius)
-		inner_diameter = 800,			--diameter of the stairs to the center of the handrail. There is an additional offset (see outer_radius)
+		diameter = 3000,			--diameter of the stairs to the center of the handrail. There is an additional offset (see outer_radius)
+		inner_diameter = 600,			--diameter of the stairs to the center of the handrail. There is an additional offset (see outer_radius)
 		sheet_thickness = 10,			--excess of the steps at the central pole and at the vertical rail bars
 		clockwise = false,				--rotation direction
 
@@ -24,31 +24,38 @@ end
 
 function test_dialog(dialog, data)
 	dialog:set_window_title("Spiral Staircase")
-	if data.material_steps == nil then 
-		btn_steps = dialog:create_button({1,2}, "Material Steps")
-	end
+
 	if data.material_glas == nil then 
 		btn_glass = dialog:create_button({3,4}, "Material Glass")
 	end
-	if data.material_frame == nil then 
-		btn_frame = dialog:create_button({1,2}, "Material Frame")
+	local btn_frame = dialog:create_button({1,2}, "Material Frame")
+	
+	local btn_steps = dialog:create_button({3,4}, "Material Steps")
+	if data.material_steps then
+		btn_steps:set_control_text(data.material_steps:get_name())
+	else 
+		btn_steps:set_control_text("Material Steps")
 	end
+	if data.material_frame then
+		btn_frame:set_control_text(data.material_frame:get_name())
+	else 
+		btn_frame:set_control_text("Material Frame")
+	end
+	
 	local align1 = dialog:create_align({1,4}) -- So that OK and Cancel will be in the same row
 	
 	local label2 = dialog:create_label(1, "Total Height")
 	local total_height = dialog:create_text_box(2, pyui.format_length(data.total_height))
-	local label1 = dialog:create_label(1, "Diameter")
-	local diameter = dialog:create_text_box(2, pyui.format_length(data.diameter))
-	local label3 = dialog:create_label(1, "No. of steps")
-	local steps = dialog:create_text_box(2, pyui.format_length(data.steps))
 	local label4 = dialog:create_label(3, "Step Thickness")
 	local step_thickness = dialog:create_text_box(4, pyui.format_length(data.step_thickness))
-	local label5 = dialog:create_label(3, "Total Angle")
-	local total_angle = dialog:create_text_box(4, pyui.format_length(data.total_angle))
+	local label1 = dialog:create_label(1, "Diameter")
+	local diameter = dialog:create_text_box(2, pyui.format_length(data.diameter))
+	local label5 = dialog:create_label(3, "Inner Diameter")
+	local diameter2 = dialog:create_text_box(4, pyui.format_length(data.inner_diameter))
 	
-	local clockwise = dialog:create_check_box({3,4}, "Clockwise")
+--	local clockwise = dialog:create_check_box({3,4}, "Clockwise")
 
-	local align1 = dialog:create_align({3,4}) -- So that OK and Cancel will be in the same row
+	local align1 = dialog:create_align({1,4}) -- So that OK and Cancel will be in the same row
 	local ok = dialog:create_ok_button(3)
 	local cancel = dialog:create_cancel_button(4)
 	
@@ -59,49 +66,45 @@ function test_dialog(dialog, data)
 		recreate_geometry(data)
 	end)
 	
+	diameter2:set_on_change_handler(function(text)
+		data.inner_diameter = pyui.parse_length(text)
+		recreate_geometry(data)
+	end)
 	
-	if data.material_steps == nil then 
-		btn_steps:set_on_click_handler(function()
-			data.material_steps = pyio.select_material(data.material_steps)
-			if data.material_steps then
-				btn_steps:set_control_text(data.material_steps:get_name())
-			end
-			recreate_geometry(data)
-		end)
-	end
+	btn_steps:set_on_click_handler(function()
+		data.material_steps = pyio.select_material(data.material_steps)
+		if data.material_steps then
+			btn_steps:set_control_text(data.material_steps:get_name())
+		else 
+			btn_steps:set_control_text("Material Steps")
+		end
+		recreate_geometry(data)
+	end)
 	
 	if data.material_glas == nil then 
 		btn_glass:set_on_click_handler(function()
 			data.material_glas = pyio.select_material(data.material_glas)
 			if data.material_glas then
 				btn_glass:set_control_text(data.material_glas:get_name())
-			end
-			recreate_geometry(data)
-		end)
-		end
-	if data.material_frame == nil then 
-		btn_frame:set_on_click_handler(function()
-			data.material_frame = pyio.select_material(data.material_frame)
-			if data.material_frame then
-				btn_frame:set_control_text(data.material_frame:get_name())
+			else 
+				btn_glass:set_control_text("Material Glass")
 			end
 			recreate_geometry(data)
 		end)
 	end
-	
-	
-	
-	
-	
+	btn_frame:set_on_click_handler(function()
+		data.material_frame = pyio.select_material(data.material_frame)
+		if data.material_frame then
+			btn_frame:set_control_text(data.material_frame:get_name())
+		else 
+			btn_frame:set_control_text("Material Frame")
+		end
+		recreate_geometry(data)
+	end)
 	
 	
 	total_height:set_on_change_handler(function(text)
 		data.total_height = pyui.parse_length(text)
-		recreate_geometry(data)
-	end)
-	
-	steps:set_on_change_handler(function(text)
-		data.steps = pyui.parse_length(text)
 		recreate_geometry(data)
 	end)
 	
@@ -110,21 +113,53 @@ function test_dialog(dialog, data)
 		recreate_geometry(data)
 	end)
 	
-	total_angle:set_on_change_handler(function(text)
-		data.total_angle = pyui.parse_length(text)
-		recreate_geometry(data)
-	end)
 	
-	clockwise:set_on_click_handler(function(state)
-		data.clockwise = state
-		recreate_geometry(data)
-	end)
+--	clockwise:set_on_click_handler(function(state)
+--		data.clockwise = state
+--		recreate_geometry(data)
+--	end)
 	
 	
 end
 
 
 function create_step(outer_radius, inner_radius, height, angle, base_height)
+	local cross_sec = {}
+	local x = 0
+	local y = 0
+	local z = base_height
+	local segs = 12
+	local length = outer_radius - inner_radius
+	local inner_angle = angle + (30) /inner_radius * 180 / math.pi
+	local outer_angle = angle + (30) /outer_radius * 180 / math.pi
+	local inner_phi = inner_angle / segs
+	local outer_phi = outer_angle / segs
+
+--here we create the points on the outside
+	for i=0,segs,1 do
+		x = outer_radius * COS(- angle / 2 - 30 /outer_radius * 180 / math.pi + i * outer_phi)
+		y = outer_radius * SIN(- angle / 2 - 30 /outer_radius * 180 / math.pi + i * outer_phi)
+		table.insert(cross_sec, {x, y,z})
+	end
+--here we create the points on the inside
+	for i=segs,0,-1 do
+		x = inner_radius * COS(- angle / 2 - 30 /inner_radius * 180 / math.pi + i * inner_phi)
+		y = inner_radius * SIN(- angle / 2 - 30 /inner_radius * 180 / math.pi + i * inner_phi)
+		table.insert(cross_sec, {x, y,z})
+	end
+	
+--first we create a face, then a profile from the face and then we delete the original face
+	local face_handle = pytha.create_polygon(cross_sec)
+	local profile = pytha.create_profile(face_handle, height)[1]
+	pytha.delete_element(face_handle)
+	pytha.create_element_ref_point(profile, {inner_radius, 0, base_height + height})
+	pytha.create_element_ref_point(profile, {outer_radius, 0, base_height + height})
+--give back the handle of the profile.
+	return profile
+end
+
+
+function create_step_base(outer_radius, inner_radius, height, angle, base_height)
 	local cross_sec = {}
 	local x = 0
 	local y = 0
@@ -147,55 +182,35 @@ function create_step(outer_radius, inner_radius, height, angle, base_height)
 	local face_handle = pytha.create_polygon(cross_sec)
 	local profile = pytha.create_profile(face_handle, height)[1]
 	pytha.delete_element(face_handle)
-	pytha.create_element_ref_point(profile, {inner_radius, 0, base_height + height})
-	pytha.create_element_ref_point(profile, {outer_radius, 0, base_height + height})
---give back the handle of the profile.
-	return profile
-end
-
-
-function create_step_base(outer_radius, inner_radius, height, angle, base_height)
-	local cross_sec = {}
-	local x = 0
-	local y = 0
-	local z = base_height
-	local segs = 12
-	local length = outer_radius - inner_radius
-	local inner_angle = angle - (5 + height) /inner_radius * 180 / math.pi
-	local outer_angle = angle - (5 + height) /outer_radius * 180 / math.pi
-	local inner_phi = inner_angle / segs
-	local outer_phi = outer_angle / segs
-
---here we create the points on the outside
-	for i=0,segs,1 do
-		x = outer_radius * COS(- angle / 2 + height /outer_radius * 180 / math.pi + i * outer_phi)
-		y = outer_radius * SIN(- angle / 2 + height /outer_radius * 180 / math.pi + i * outer_phi)
-		table.insert(cross_sec, {x, y,z})
-	end
---here we create the points on the inside
-	for i=segs,0,-1 do
-		x = inner_radius * COS(- angle / 2 + height /inner_radius * 180 / math.pi + i * inner_phi)
-		y = inner_radius * SIN(- angle / 2 + height /inner_radius * 180 / math.pi + i * inner_phi)
-		table.insert(cross_sec, {x, y,z})
-	end
---first we create a face, then a profile from the face and then we delete the original face
-	local face_handle = pytha.create_polygon(cross_sec)
-	local profile = pytha.create_profile(face_handle, height)[1]
-	pytha.delete_element(face_handle)
 --give back the handle of the profile.
 	return profile
 end
 
 function recreate_geometry(data)
-	local step_height = data.total_height / data.steps	
-	local pole_section_height = step_height - data.step_thickness	--height of the central pole pieces
 	local outer_radius = data.diameter / 2	--this is the actual radius of the individual steps
+	local inner_radius = data.inner_diameter / 2	
+	--firstly calculate number of steps, depending on the total height
+	data.steps = math.floor(data.total_height / 180 + 0.5)	--round to next integer
+	local step_height = data.total_height / data.steps	--as close as possible to 180mm
+	
+	local radius_of_walking = (outer_radius - inner_radius) * 2 / 3		--at 2/3 of the stair width
+	local step_length = 630 - 2 * step_height
+	local approximate_angle = ATAN(step_length / radius_of_walking)
+	--now we can get this angle to giving a full number of steps on 360 deg
+	local step_angle = 360 / math.floor(360 / approximate_angle + 0.5) 
+	
+	
+	data.total_angle = data.steps * step_angle
+
+
+
+
+	local pole_section_height = step_height - data.step_thickness	--height of the central pole pieces
 	if data.cur_elements ~= nil then
 		pytha.delete_element(data.cur_elements)
 	end
 	data.cur_elements = {}
 
-	local step_angle = data.total_angle / data.steps
 
 	local step = create_step(outer_radius - 35, data.inner_diameter / 2 + 10, data.step_thickness, step_angle, pole_section_height)
 	if data.material_steps ~= nil then pytha.set_element_material(step, data.material_steps) end
@@ -232,7 +247,7 @@ function recreate_geometry(data)
 		local y = data.diameter / 2 * SIN(data.total_angle * i / segs - phi_off)
 		local z = data.rail_height + step_height  + i * data.total_height / (segs)
 		table.insert(points, {x, y, z})
-		z = i * data.total_height / (segs)	--this is the negative position of the outer steel construction
+		z = 50 + i * data.total_height / (segs)	--this is the negative position of the outer steel construction
 		table.insert(points2, {x, y, z})
 	end
 	local rail_edges = pytha.create_polyline("open", points)	--create the polyline
@@ -257,16 +272,19 @@ function recreate_geometry(data)
 	pytha.delete_element(rail2)
 
 
-
-	for i=-1,segs + 1,1 do		
+	for i=-7,segs + 5,1 do		
 		local x = data.inner_diameter / 2 * COS(data.total_angle * i / segs - phi_off)
 		local y = data.inner_diameter / 2 * SIN(data.total_angle * i / segs - phi_off)
 		local z = data.rail_height / 2 - 200 + step_height + i * data.total_height / (segs)	--this is the negative position of the outer steel construction
 		table.insert(inner_points, {x, y, z})
+
+	end
+
+	for i=-1,segs + 1,1 do		
 		
 		x = data.diameter / 2 * COS(data.total_angle * i / segs - phi_off)
 		y = data.diameter / 2 * SIN(data.total_angle * i / segs - phi_off)
-		z = (data.rail_height + step_height - 20 + 200) / 2 + i * data.total_height / (segs)	--this is the negative position of the outer steel construction
+		z = (data.rail_height + step_height - 20 + 250) / 2 + i * data.total_height / (segs)	--this is the negative position of the outer steel construction
 		table.insert(points3, {x, y, z})
 	end
 	local inner_rail_edges = pytha.create_polyline("open", inner_points)	--create the polyline
@@ -286,7 +304,7 @@ function recreate_geometry(data)
 
 	rail_edges = pytha.create_polyline("open", points3)	--create the polyline
 
-	sweep_cross_section = {type = "rectangle", length = 10, width = data.rail_height + step_height - 20 - 200} --data for the inner cross section
+	sweep_cross_section = {type = "rectangle", length = 10, width = data.rail_height + step_height - 20 - 250} 
 	sweep_options = {keep_vertical = 1}
 	rail = pytha.create_sweep(rail_edges, sweep_cross_section, sweep_options)[1]		--create the sweep
 	if data.material_glas ~= nil then pytha.set_element_material(rail, data.material_glas) end
