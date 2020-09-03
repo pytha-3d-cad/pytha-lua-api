2074b8a8dd4d42e379a331b2e48ba6937309d11e
fabian-flassig <64731310+fabian-flassig@users.noreply.github.com>
2020-09-03 09:59:26 +0200

    New cube sample

--- samples/Cube-Sample/cube-sample.lua
+++ samples/Cube-Sample/cube-sample.lua
@@ -1,10 +1,29 @@
---Beispiel eines einfachen Schrankkorpus mit variabler Anzahl an Fachboeden
+--Simple block example with history
+
+function eval(element)
+	local attribute = pytha.get_element_attribute(element, "block_special_attribute")
+	local name = pytha.get_element_attribute(element, "name")
+	local dimensions = pytha.get_element_bounding_box(element)
+	local length = pytha.get_length_unit() 
+	return attribute .. " " .. name .. "! Your length is " .. pyui.format_length(dimensions[2][1] - dimensions[1][1]) .. length
+end
+
+function edit_block(element)
+	loaded_data = pytha.get_element_history(element, "block_history_demo")
+	if loaded_data == nil then
+		pyui.alert(pyloc "No data found")
+		return 
+	end
+	recreate_geometry(loaded_data)
+	pyui.run_modal_dialog(cube_dialog, loaded_data)
+	pyio.save_values("default_dimensions", loaded_data)
+end
 
 function main()
-	local data = {size = 200., name = "Box"}
+	local data = {size = 200., name = "Box", pre_text = "Hello"}
 	
-	local loaded_data = pyio.load_values("default_dimensions")
-	if loaded_data ~= nil then data = loaded_data end
+--	local loaded_data = pyio.load_values("default_dimensions")
+--	if loaded_data ~= nil then data = loaded_data end
 	recreate_geometry(data)
 	
 	pyui.run_modal_dialog(cube_dialog, data)
@@ -15,11 +34,13 @@ end
 function cube_dialog(dialog, data)
 	dialog:set_window_title("My Cube")
 	
-	local label1 = dialog:create_label(1, "Size", {align = "left"})
+	local label1 = dialog:create_label(1, "Size")
 	local size = dialog:create_text_box(2, pyui.format_length(data.size))
 	local btn = dialog:create_button({3,4}, "Material")
-	local label2 = dialog:create_label({1,2}, "Name", {align = "left"})
+	local label2 = dialog:create_label({1,2}, "Name")
 	local name = dialog:create_text_box({3,4}, data.name)
+	local label3 = dialog:create_label({1,2}, "Special Cube Greeting")
+	local spec_attribute = dialog:create_text_box({3,4}, data.pre_text)
 	
 	dialog:create_align({1,4})
 	local ok = dialog:create_ok_button({1,2})
@@ -40,8 +61,13 @@ function cube_dialog(dialog, data)
         recreate_geometry(data)
     end)
 	
+    spec_attribute:set_on_change_handler(function(text)
+        data.pre_text = text
+        recreate_geometry(data)
+    end)
+	
     btn:set_on_click_handler(function()
-        data.mat_handle = pyux.select_pyo(data.mat_handle)
+        data.mat_handle = pyux.select_material(data.mat_handle)
 		if data.mat_handle then
 			btn:set_control_text(data.mat_handle:get_name())
 		end
@@ -55,23 +81,13 @@ function recreate_geometry(data)
     if data.current_element ~= nil then
         pytha.delete_element(data.current_element)
     end
-    if data.imported_parts ~= nil then
-        pytha.delete_element(data.imported_parts)
-    end
+
 	
-	data.current_element = pytha.create_cylinder(data.size, data.size, {0,0,0})
+	data.current_element = pytha.create_block(data.size, data.size, data.size, {0,0,0}, {name = data.name})
+	
+	data.current_element:set_element_attributes({block_special_attribute = data.pre_text})
 	if data.mat_handle then
-		data.imported_parts = pytha.import_pyo(data.mat_handle, {0,0,500},{u_axis = {1,1,0}, v_axis = {-1,1,1}},{Length = 1000, width = 3000})
+		pytha.set_element_material(data.current_element, data.mat_handle) 
 	end
-	
-	local refpt = pytha.get_element_bounding_box(data.current_element)
-	
-	for i,k in pairs(refpt) do
-		for j,l in pairs(k) do
---			pyui.alert(l)
-		end
-	end
-		
-	
-	
+	pytha.set_element_history(data.current_element, data, "block_history_demo")
 end
