d7ee170f7f540acb92160dae55cd802ae2c198f3
fabian-flassig <64731310+fabian-flassig@users.noreply.github.com>
2023-02-16 11:57:24 +0100

    Random rotation sample added
    A small plugin has been added that randomly rotates objects around their centers.

---
+++ samples/Random Rotation/main.lua
@@ -0,0 +1,17 @@
+--Randomly rotate objects around their individual centers
+function main()
+
+	local parts = pyux.select_part(true, "Select parts to be randomly rotated")
+	if parts == nil then return end
+
+	for i,k in ipairs(parts) do
+		local bbox = pytha.get_element_bounding_box(k)
+		local center = {(bbox[1][1] + bbox[2][1]) / 2, (bbox[1][2] + bbox[2][2]) / 2, (bbox[1][3] + bbox[2][3]) / 2}
+		local angle = math.random() * 360
+		local axis = {math.random(), math.random(), math.random()}
+		local len = PYTHAGORAS(axis[1], axis[2], axis[3])
+		axis = {axis[1]/len, axis[2]/len, axis[3]/len}
+		pytha.rotate_element(k, center, axis, angle)
+	end
+
+end
