a928e6535c4d22ecc01aac8bf775437c2be09624
fabian-flassig <64731310+fabian-flassig@users.noreply.github.com>
2023-02-17 16:29:58 +0100

    Modified the axis random value ranges

--- samples/Random Rotation/main.lua
+++ samples/Random Rotation/main.lua
@@ -8,7 +8,7 @@ function main()
 		local bbox = pytha.get_element_bounding_box(k)
 		local center = {(bbox[1][1] + bbox[2][1]) / 2, (bbox[1][2] + bbox[2][2]) / 2, (bbox[1][3] + bbox[2][3]) / 2}
 		local angle = math.random() * 360
-		local axis = {math.random(), math.random(), math.random()}
+		local axis = {-1 + 2 * math.random(), -1 + 2 * math.random(), -1 + 2 * math.random()}
 		local len = PYTHAGORAS(axis[1], axis[2], axis[3])
 		axis = {axis[1]/len, axis[2]/len, axis[3]/len}
 		pytha.rotate_element(k, center, axis, angle)
